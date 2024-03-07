4c25354318c78c67fb18ef6fe915acb29968df89
pretzelogical <yingyangyo266@gmail.com>
2024-03-07 10:01:27 -0600

    task 6

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { schema } = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb://localhost:27017/test', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
 
 app.use(
   '/graphql',
@@ -11,6 +16,10 @@ app.use(
     graphiql: true
   })
 );
+
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 app.listen(4000, () => {
   console.log('now listening for request on port 4000');
